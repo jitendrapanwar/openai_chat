7cf5f8ecf58534c10f0fd94086848b8971683ac7
Jitendra Panwar <gm.jitendra@gmail.com>
2026-06-28 21:15:35 +0530

    run both apps together

--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
-console.log("Hello via Bun!");
+import concurrently from "concurrently";
+concurrently([
+  {
+    name: "server",
+    command: "npm run dev",
+    cwd: "packages/server",
+    prefixColor: "blue",
+  },
+  {
+    name: "client",
+    command: "npm run dev",
+    cwd: "packages/client",
+    prefixColor: "green",
+  },
+]);
